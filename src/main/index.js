630c726133043741cf91aefd30cacd02173ff7e0
long.woo <longwunet@gmail.com>
2017-12-10 17:26:56 +0800

    测试

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
+import { autoUpdater } from 'electron-updater'
 
 /**
  * Set `__static` path to static files in production
@@ -32,7 +33,17 @@ function createWindow () {
   })
 }
 
-app.on('ready', createWindow)
+/**
+ * 发送自动更新相关状态
+ * @param {*} text 更新描述
+ */
+function sendAutoUpdateStatus (text) {
+  mainWindow.webContents.send('autoUpdateStatus', text)
+}
+
+ipcMain.on('checkUpdate', (event, arg) => {
+  autoUpdater.checkForUpdates()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -53,15 +64,36 @@ app.on('activate', () => {
  * support auto updating. Code Signing with a valid certificate is required.
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
+autoUpdater.on('checking-for-update', () => {
+  sendAutoUpdateStatus('正在检查更新...')
+})
 
-/*
-import { autoUpdater } from 'electron-updater'
+autoUpdater.on('update-available', info => {
+  sendAutoUpdateStatus('发现新版本～，开始下载...')
+  // autoUpdater.downloadUpdate()
+})
+
+autoUpdater.on('update-not-available', info => {
+  sendAutoUpdateStatus('已经是最新版本~')
+})
+
+autoUpdater.on('error', info => {
+  sendAutoUpdateStatus(`更新出错：${info}`)
+})
+
+autoUpdater.on('download-progress', (processObj) => {
+  // let text = `下载速度${processObj.bytesPerSecond}，已下载${processObj.percent}%（${processObj.transferred}/${processObj.total}）`
+  let text = `已下载${parseInt(processObj.percent)}%`
+
+  sendAutoUpdateStatus(text)
+})
 
 autoUpdater.on('update-downloaded', () => {
   autoUpdater.quitAndInstall()
 })
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+  createWindow()
+
+  // if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -23,6 +23,10 @@
           <div class="title alt">Other Documentation</div>
           <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
           <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+          <p>
+            <a href="javascript:;" @click="checkUpdate">检查更新</a>
+          </p>
+          <p>{{updateStatus}}</p>
         </div>
       </div>
     </main>
@@ -35,9 +39,22 @@
   export default {
     name: 'landing-page',
     components: { SystemInformation },
+    data () {
+      return {
+        updateStatus: ''
+      }
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
+      },
+      checkUpdate () {
+        const ipcRender = this.$electron.ipcRenderer
+
+        ipcRender.send('checkUpdate')
+        ipcRender.on('autoUpdateStatus', (event, res) => {
+          this.updateStatus = res
+        })
       }
     }
   }
